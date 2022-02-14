d77888c931185266bc8d41a115080e75cfb5855a
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-02-14 16:38:00 +0100

    Format with black

--- test_rpn_interpreter.py
+++ test_rpn_interpreter.py
@@ -6,23 +6,32 @@ from rpn_interpreter import minus
 from rpn_interpreter import divide
 from rpn_interpreter import Stack
 import pytest
+
+
 def test_single_value():
     assert evaluate("42") == 42
 @pytest.mark.skip
+
+
 def test_single_expression():
     assert evaluate("20 2 +") == 22
+
 
 def test_parse_empty():
     assert parse("") == []
 
+
 def test_parse_single_value():
     assert parse("45") == [45]
+
 
 def test_parse_two_numbers():
     assert parse("1 2") == [1, 2]
 
+
 def test_parse_add():
     assert parse("1 2 +") == [1, 2, add]
+
 
 def test_parse_multiply():
     assert parse("1 2 *") == [1, 2, multiply]
