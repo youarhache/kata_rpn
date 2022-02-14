8ac3ece055cafe9a2423e660b9693e88294c0799
nadya <nadya.sadfi@arolla.fr>
2022-02-14 14:37:29 +0100

    impliment parsing

--- rpn_interpreter.py
+++ rpn_interpreter.py
@@ -1,3 +1,20 @@
 def evaluate(expression):
     result = int(expression)
     return result
+def parse(expression):
+    if not expression:
+        return []
+    tokens = [parse_token(x) for x in expression.split(" ")]
+    return tokens
+def parse_token(token):
+    if token == "+":
+        return add
+    elif token == "*":
+        return multiply
+    return int(token)
+
+def add():
+    pass
+
+def multiply():
+    pass
--- test_rpn_interpreter.py
+++ test_rpn_interpreter.py
@@ -1,5 +1,25 @@
 from rpn_interpreter import evaluate
-
-
+from rpn_interpreter import parse
+from rpn_interpreter import add
+from rpn_interpreter import multiply
+import pytest
 def test_single_value():
     assert evaluate("42") == 42
+@pytest.mark.skip
+def test_single_expression():
+    assert evaluate("20 2 +") == 22
+
+def test_parse_empty():
+    assert parse("") == []
+
+def test_parse_single_value():
+    assert parse("45") == [45]
+
+def test_parse_two_numbers():
+    assert parse("1 2") == [1, 2]
+
+def test_parse_add():
+    assert parse("1 2 +") == [1, 2, add]
+
+def test_parse_multiply():
+    assert parse("1 2 *") == [1, 2, multiply]
